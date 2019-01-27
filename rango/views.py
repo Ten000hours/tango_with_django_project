9ff57e5ecf9a28fe0f5f2190242c79a2bd1ad183
Ten000hours <502250806@qq.com>
2019-01-27 20:58:07 +0000

    chapter 6

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,7 @@ from rango.models import Category,Page
 def index(request):
     # construct a dict to pass to the template engine as its context
     # note the key boldmessage is the same as {{boldmessage}} in the template
-    category_list = Category.objects.order_by('name')[:5]
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
 
     #return a rendered response to send to the client
@@ -12,3 +12,26 @@ def index(request):
     # note that  the first parameter is the template we wish to use
 
     return render(request,"rango/index.html",context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        # Retrieve all of the associated pages.
+        # Note that filter() will return a list of page objects or an empty list
+        pages = Page.objects.filter(category=category)
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+        # We also add the category object from
+        # the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything -
+            context_dict['category'] = None
+            context_dict['pages'] = None
+    return render(request, 'rango/category.html', context_dict)
+def about(request, category_name_slug):
+    context_dict={}
+    return render(request, 'rango/category.html', context_dict)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,6 +24,12 @@ urlpatterns = [
     # url(r'^rango/', include('rango.urls')),
     # url(r'^admin/', admin.site.urls),
     # #path('admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
+
+
+    # url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category, name='show_category'),
 ]
