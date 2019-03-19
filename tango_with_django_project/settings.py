2dbecd495f058f5ac9220122f02fb2888d17f0bd
Ten000hours <502250806@qq.com>
2019-03-19 09:54:59 +0000

    update the item.html and showitem.html

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
-    'geoposition',
+
     "registration",
     "crispy_forms",
     'threadedcomments',
